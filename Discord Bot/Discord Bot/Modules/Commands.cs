18c873f5939f1daebd6ba34d0d355a6d53466cf3
castagnola.nicolas <castagnola.nicolas@gmail.com>
2021-09-23 07:27:28 -0300

    1.0

---
+++ Discord Bot/Discord Bot/Modules/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using HtmlAgilityPack;
+
+namespace Discord_Bot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+
+    }
+}
